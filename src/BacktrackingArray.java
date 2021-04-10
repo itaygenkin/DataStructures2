9afd9a0ff6f35d1a6260c2a162b9ffe9d5a08dca
Itay Genkin <itay.genkin@gmail.com>
2021-04-10 18:54:33 +0300

    _init_

--- src/BacktrackingArray.java
+++ src/BacktrackingArray.java
@@ -4,6 +4,7 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     private Stack stack;
     private int[] arr;
     // TODO: implement your code here
+    private int inserted = 0;
 
     // Do not change the constructor's signature
     public BacktrackingArray(Stack stack, int size) {
@@ -14,52 +15,114 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     @Override
     public Integer get(int index){
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+    	return arr[index]; // temporal return command to prevent compilation error
     }
 
     @Override
     public Integer search(int k) {
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        for (int i=0; i<inserted; i++){
+            if (arr[i] == k)
+                return i;
+        }
+    	return -1; // temporal return command to prevent compilation error
     }
 
     @Override
-    public void insert(Integer x) {
+    public void insert(Integer x) { //What to do if arr is full?
         // TODO: implement your code here
+        if (inserted >= arr.length)
+            throw new IllegalArgumentException("The array is full!");
+        stack.push(x);
+        stack.push(inserted + 1);
+        arr[inserted] = x;
+        inserted = inserted + 1;
     }
 
     @Override
     public void delete(Integer index) {
         // TODO: implement your code here
+        if (index >= arr.length)
+            throw new IllegalArgumentException("Index out of bounds");
+        stack.push(arr[index]);
+        stack.push(index * (-1) - 1);
+        for (int i=index; i<inserted-1; i++){
+            arr[i] = arr[i+1];
+        }
+        inserted = inserted - 1;
     }
 
     @Override
     public Integer minimum() {
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        if (inserted == 0){
+            throw new IllegalArgumentException("There is no value");
+        }
+        int min = 0;
+        for (int i=1; i<inserted; i++){
+            if ( arr[min] > arr[i] )
+                min = i;
+        }
+    	return min; // temporal return command to prevent compilation error
     }
 
     @Override
     public Integer maximum() {
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        if (inserted == 0){
+            throw new IllegalArgumentException("There is no value");
+        }
+        int max = 0;
+        for (int i=1; i<inserted; i++){
+            if ( arr[max] < arr[i] )
+                max = i;
+        }
+    	return max; // temporal return command to prevent compilation error
     }
 
     @Override
     public Integer successor(Integer index) {
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        if (inserted < index || index < 0 || inserted < 2 || this.maximum() == arr[index])
+            throw new IllegalArgumentException("Wrong input");
+        int output = maximum();
+        for (int i=0; i<inserted; i++){
+            if (arr[index] < arr[i] && arr[i] < arr[output])
+                output = i;
+        }
+    	return output; // temporal return command to prevent compilation error
     }
 
     @Override
     public Integer predecessor(Integer index) {
         // TODO: implement your code here
-    	return null; // temporal return command to prevent compilation error
+        if (inserted < index || index < 0 || inserted < 2 || this.minimum() == arr[index])
+            throw new IllegalArgumentException("Wrong input");
+        int output = minimum();
+        for (int i=0; i<inserted; i++){
+            if (arr[index] > arr[i] && arr[i] > arr[output])
+                output = i;
+        }
+    	return output; // temporal return command to prevent compilation error
     }
 
     @Override
     public void backtrack() {
         // TODO: implement your code here
+        if ( !stack.isEmpty() ){
+            int index = (int) stack.pop();
+            int value = (int) stack.pop();
+            if ( index > 0 )
+                delete(index-1);
+            else{
+                index = (index + 1) * (-1);
+                for (int i=inserted; i>index; i--){
+                    arr[i+1] = arr[i];
+                }
+                arr[index] = value;
+                inserted = inserted + 1;
+            }
+        }
     }
 
     @Override
@@ -72,6 +135,13 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     @Override
     public void print() {
         // TODO: implement your code here
+        String str = "";
+        for (int i=0; i<inserted; i++){
+                str = str + Integer.toString(arr[i]) + " ";
+        }
+        if (inserted > 0)
+            str = str.substring(0, str.length() - 1);
+        System.out.println(str);
     }
     
 }
